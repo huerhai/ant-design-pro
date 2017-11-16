d417320c1d24e1df236afde71a80d8c0b275bae2
Wenjie Li <liwenjie@leapstack.cn>
2017-11-16 16:27:11 +0800

    修复dva异步加载的BUG~

--- src/common/nav.js
+++ src/common/nav.js
@@ -1,114 +1,253 @@
+import dynamic from 'dva/dynamic';
+
+// models
+import UserModel from '../models/user';
+import ChartModel from '../models/chart';
+import MonitorModel from '../models/monitor';
+import ProjectModel from '../models/project';
+import ActivitiesModel from '../models/activities';
+import FormModel from '../models/form';
+import RuleModel from '../models/rule';
+import ListModel from '../models/list';
+import ProfileModel from '../models/profile';
+import LoginModel from '../models/login';
+import RegisterModel from '../models/register';
+
+// components
 import BasicLayout from '../layouts/BasicLayout';
-// import UserLayout from '../layouts/UserLayout';
-// import BlankLayout from '../layouts/BlankLayout';
-//
-// import Analysis from '../routes/Dashboard/Analysis';
-// import Monitor from '../routes/Dashboard/Monitor';
-// import Workplace from '../routes/Dashboard/Workplace';
+import UserLayout from '../layouts/UserLayout';
+import BlankLayout from '../layouts/BlankLayout';
+
+import Analysis from '../routes/Dashboard/Analysis';
+import Monitor from '../routes/Dashboard/Monitor';
+import Workplace from '../routes/Dashboard/Workplace';
+
+import BasicForm from '../routes/Forms/BasicForm';
+import AdvancedForm from '../routes/Forms/AdvancedForm';
+import StepForm from '../routes/Forms/StepForm';
+import Step2 from '../routes/Forms/StepForm/Step2';
+import Step3 from '../routes/Forms/StepForm/Step3';
 
 import TableList from '../routes/List/TableList';
-// import CoverCardList from '../routes/List/CoverCardList';
-// import CardList from '../routes/List/CardList';
-// import FilterCardList from '../routes/List/FilterCardList';
-// import SearchList from '../routes/List/SearchList';
-// import BasicList from '../routes/List/BasicList';
-//
-// import BasicProfile from '../routes/Profile/BasicProfile';
-// import AdvancedProfile from '../routes/Profile/AdvancedProfile';
-//
-// import BasicForm from '../routes/Forms/BasicForm';
-// import AdvancedForm from '../routes/Forms/AdvancedForm';
-// import StepForm from '../routes/Forms/StepForm';
-// import Step2 from '../routes/Forms/StepForm/Step2';
-// import Step3 from '../routes/Forms/StepForm/Step3';
-//
-// import Exception403 from '../routes/Exception/403';
-// import Exception404 from '../routes/Exception/404';
-// import Exception500 from '../routes/Exception/500';
-//
-// import Success from '../routes/Result/Success';
-// import Error from '../routes/Result/Error';
-//
-// import Login from '../routes/User/Login';
-// import Register from '../routes/User/Register';
-// import RegisterResult from '../routes/User/RegisterResult';
+import BasicList from '../routes/List/BasicList';
+import CardList from '../routes/List/CardList';
+import CoverCardList from '../routes/List/CoverCardList';
+import FilterCardList from '../routes/List/FilterCardList';
+import SearchList from '../routes/List/SearchList';
 
-import EntryForm from '../routes/EntrySystem/entry';
+import BasicProfile from '../routes/Profile/BasicProfile';
+import AdvancedProfile from '../routes/Profile/AdvancedProfile';
 
-import Company from '../routes/BasicInfo/company';
-import Group from '../routes/BasicInfo/group';
-import Policy from '../routes/BasicInfo/policy';
+import Success from '../routes/Result/Success';
+import Error from '../routes/Result/Error';
+import Exception403 from '../routes/Exception/403';
+import Exception404 from '../routes/Exception/404';
+import Exception500 from '../routes/Exception/500';
 
-const data = [{
-  component: BasicLayout,
-  layout: 'BasicLayout',
-  name: '首页', // for breadcrumb
-  path: '/',
-  children: [{
-    name: '录入系统',
-    path: 'entry',
-    icon: 'form',
-    children: [{
-      name: '影像件导入',
-      path: 'todo',
-      component: TableList,
-    }, {
-      name: '数据导入',
-      path: '录入',
-      component: EntryForm,
-    }],
-  }, {
-    name: '栈略数据',
-    path: 'leapStack',
-    icon: 'form',
-    children: [{
-      name: '待审核',
-      path: 'todo',
-      component: TableList,
-    }, {
-      name: '已审核',
-      path: 'approved',
-      component: TableList,
-    }],
-  }, {
-    name: '基础信息',
-    path: 'basicInfo',
-    icon: 'form',
-    children: [{
-      name: '保险公司管理',
-      path: 'company',
-      component: Company,
-    }, {
-      name: '产品管理',
-      path: 'product',
-      component: TableList,
-    }, {
-      name: '团体管理',
-      path: 'Group',
-      component: Group,
-    }, {
-      name: '保单管理',
-      path: 'Policies',
-      component: Policy,
-    }],
-  }, {
-    name: '易安保险公司',
-    path: 'yian',
-    icon: 'form',
-    children: [{
-      name: '待审核',
-      path: 'Check',
-      component: TableList,
-    }, {
-      name: '已审核',
-      path: 'Archiving',
-      component: TableList,
-    }],
-  }],
-}];
+import Login from '../routes/User/Login';
+import Register from '../routes/User/Register';
+import RegisterResult from '../routes/User/RegisterResult';
 
-export function getNavData() {
-  return data;
-}
+// wrapper of dynamic
+const dy = (app, models, component) => dynamic({
+  app,
+  models: () => models,
+  component: () => component,
+});
 
-export default data;
+// nav data
+export const getNavData = app => [
+  {
+    component: dy(app, [UserModel], BasicLayout),
+    layout: 'BasicLayout',
+    name: '首页', // for breadcrumb
+    path: '/',
+    children: [
+      {
+        name: 'Dashboard',
+        icon: 'dashboard',
+        path: 'dashboard',
+        children: [
+          {
+            name: '分析页',
+            path: 'analysis',
+            component: dy(app, [ChartModel], Analysis),
+          },
+          {
+            name: '监控页',
+            path: 'monitor',
+            component: dy(app, [MonitorModel], Monitor),
+          },
+          {
+            name: '工作台',
+            path: 'workplace',
+            component: dy(app, [ProjectModel, ActivitiesModel, ChartModel], Workplace),
+          },
+        ],
+      },
+      {
+        name: '表单页',
+        path: 'form',
+        icon: 'form',
+        children: [
+          {
+            name: '基础表单',
+            path: 'basic-form',
+            component: dy(app, [FormModel], BasicForm),
+          },
+          {
+            name: '分步表单',
+            path: 'step-form',
+            component: dy(app, [FormModel], StepForm),
+            children: [
+              {
+                path: 'confirm',
+                component: dy(app, [FormModel], Step2),
+              },
+              {
+                path: 'result',
+                component: dy(app, [FormModel], Step3),
+              },
+            ],
+          },
+          {
+            name: '高级表单',
+            path: 'advanced-form',
+            component: dy(app, [FormModel], AdvancedForm),
+          },
+        ],
+      }, {
+        name: '列表页',
+        path: 'list',
+        icon: 'table',
+        children: [
+          {
+            name: '查询表格',
+            path: 'table-list',
+            component: dy(app, [RuleModel], TableList),
+          },
+          {
+            name: '标准列表',
+            path: 'basic-list',
+            component: dy(app, [ListModel], BasicList),
+          },
+          {
+            name: '卡片列表',
+            path: 'card-list',
+            component: dy(app, [ListModel], CardList),
+          },
+          {
+            name: '搜索列表（项目）',
+            path: 'cover-card-list',
+            component: dy(app, [ListModel], CoverCardList),
+          },
+          {
+            name: '搜索列表（应用）',
+            path: 'filter-card-list',
+            component: dy(app, [ListModel], FilterCardList),
+          },
+          {
+            name: '搜索列表（文章）',
+            path: 'search',
+            component: dy(app, [ListModel], SearchList),
+          },
+        ],
+      },
+      {
+        name: '详情页',
+        path: 'profile',
+        icon: 'profile',
+        children: [
+          {
+            name: '基础详情页',
+            path: 'basic',
+            component: dy(app, [ProfileModel], BasicProfile),
+          },
+          {
+            name: '高级详情页',
+            path: 'advanced',
+            component: dy(app, [ProfileModel], AdvancedProfile),
+          },
+        ],
+      },
+      {
+        name: '结果',
+        path: 'result',
+        icon: 'check-circle-o',
+        children: [
+          {
+            name: '成功',
+            path: 'success',
+            component: dy(app, [], Success),
+          },
+          {
+            name: '失败',
+            path: 'fail',
+            component: dy(app, [], Error),
+          },
+        ],
+      },
+      {
+        name: '异常',
+        path: 'exception',
+        icon: 'warning',
+        children: [
+          {
+            name: '403',
+            path: '403',
+            component: dy(app, [], Exception403),
+          },
+          {
+            name: '404',
+            path: '404',
+            component: dy(app, [], Exception404),
+          },
+          {
+            name: '500',
+            path: '500',
+            component: dy(app, [], Exception500),
+          },
+        ],
+      },
+    ],
+  },
+  {
+    component: dy(app, [], UserLayout),
+    path: '/user',
+    layout: 'UserLayout',
+    children: [
+      {
+        name: '帐户',
+        icon: 'user',
+        path: 'user',
+        children: [
+          {
+            name: '登录',
+            path: 'login',
+            component: dy(app, [LoginModel], Login),
+          },
+          {
+            name: '注册',
+            path: 'register',
+            component: dy(app, [RegisterModel], Register),
+          },
+          {
+            name: '注册结果',
+            path: 'register-result',
+            component: dy(app, [], RegisterResult),
+          },
+        ],
+      },
+    ],
+  },
+  {
+    component: dy(app, [], BlankLayout),
+    layout: 'BlankLayout',
+    children: {
+      name: '使用文档',
+      path: 'http://pro.ant.design/docs/getting-started',
+      target: '_blank',
+      icon: 'book',
+    },
+  },
+];
