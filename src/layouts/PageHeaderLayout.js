2e3dfe43ec3a0ef8db056101d4304ffb07b31f5b
Wenjie Li <liwenjie@leapstack.cn>
2017-12-02 07:50:50 +0800

    取消面包屑布局~ 节约空间

--- src/layouts/PageHeaderLayout.js
+++ src/layouts/PageHeaderLayout.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Link } from 'dva/router';
-import PageHeader from '../components/PageHeader';
 import styles from './PageHeaderLayout.less';
 
-export default ({ children, wrapperClassName, top, ...restProps }) => (
+export default ({ children, wrapperClassName, top }) => (
   <div style={{ margin: '-24px -24px 0' }} className={wrapperClassName}>
     {top}
-    <PageHeader {...restProps} linkElement={Link} />
     {children ? <div className={styles.content}>{children}</div> : null}
   </div>
 );
